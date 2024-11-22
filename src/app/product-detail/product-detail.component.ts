048505261afe14c0d34afb7b90f61d7de9feb88d
A B M Asadullah <abmasadullah@gmail.com>
2024-11-22 18:38:02 +0600

    Module complete

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { CartService } from '../services/cart.service';
+import { CommonModule } from '@angular/common';
+import {
+    RouterModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+} from '@angular/router';
 
 @Component({
-  selector: 'app-product-detail',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+    selector: 'app-product-detail',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule,
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+    ],
+    templateUrl: './product-detail.component.html',
+    styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent {
+    product: any;
+    isInCart: boolean = false;
 
+    constructor(
+        private route: ActivatedRoute,
+        private productService: ProductService,
+        private cartService: CartService
+    ) { }
+
+    ngOnInit(): void {
+        const productId = this.route.snapshot.paramMap.get('id');
+        if (productId) {
+            this.productService.getProduct(productId).subscribe((product) => {
+                this.product = product;
+                this.isInCart = this.cartService.isInCart(productId);
+            });
+        }
+    }
+
+    addToCart(): void {
+        if (this.product) {
+            this.cartService.addItem(this.product.id);
+            this.isInCart = true;
+        }
+    }
 }
